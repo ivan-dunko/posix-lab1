cd6eb15a4584d1599b8e089f837ade8d13c6f90c
ivan-dunko <54901330+ivan-dunko@users.noreply.github.com>
2020-09-08 14:50:09 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -15,17 +15,17 @@ void exitWithFailure(const char *msg){
 }
 
 void *routine(void *data){
-	for (int i = 0; i < PRINT_CNT; ++i)
-		if (write(STDIN_FILENO, THREAD_MSG, strlen(THREAD_MSG)) == -1)
+    for (int i = 0; i < PRINT_CNT; ++i)
+        if (write(STDIN_FILENO, THREAD_MSG, strlen(THREAD_MSG)) == -1)
             exitWithFailure("routine");
 
 	return NULL;
 }
 
 int main(int argc, char **argv){
-	pthread_t pid;
+    pthread_t pid;
 
-	if ((errno = pthread_create(&pid, NULL, routine, NULL) == -1)
+    if ((errno = pthread_create(&pid, NULL, routine, NULL) == -1)
 #ifndef LAB2
         || (errno = pthread_detach(pid) == -1)
 #endif
@@ -37,8 +37,8 @@ int main(int argc, char **argv){
         exitWithFailure("main");
 #endif
 
-	for (int i = 0; i < PRINT_CNT; ++i)
-		if (write(STDIN_FILENO, MAIN_MSG, strlen(MAIN_MSG)) == -1)
+    for (int i = 0; i < PRINT_CNT; ++i)
+        if (write(STDIN_FILENO, MAIN_MSG, strlen(MAIN_MSG)) == -1)
             exitWithFailure("main");
 
     pthread_exit((void*)(EXIT_SUCCESS));
